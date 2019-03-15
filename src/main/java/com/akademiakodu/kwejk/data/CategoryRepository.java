074208a2985a8e1690a0e9fe1ec858b831e18f5a
AstralEden <agataa.trzaska@gmail.com>
2019-03-15 14:54:36 +0100

    innit

---
+++ src/main/java/com/akademiakodu/kwejk/data/CategoryRepository.java
@@ -0,0 +1,24 @@
+package com.akademiakodu.kwejk.data;
+
+import com.akademiakodu.kwejk.model.Category;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class CategoryRepository {
+public static final List<Category> ALL_CATEGORIES = Arrays.asList(
+        new Category(0, "funny"),
+        new Category(1, "sport"),
+        new Category(2, "it")
+);
+    public static List<Category> getAllCategories() {
+        return ALL_CATEGORIES;
+    }
+
+    public static Category getCategoryById(int id){
+        return ALL_CATEGORIES.get(id);
+    }
+}
--- src/main/java/com/akademiakodu/kwejk/data/GifRepository.java
+++ src/main/java/com/akademiakodu/kwejk/data/GifRepository.java
@@ -10,26 +10,36 @@ import java.util.List;
 @Component//tworzy bina, czyli obiekt ktory bedziemy wstrzykiwac w inna klase
 public class GifRepository {
 
-public final static List<Gif> ALL_GIFS= Arrays.asList(
-        new Gif("infinite-andrew", "USERNAME", true, 1),
-        new Gif("cowboy-coder", "USERNAME", false, 2),
-        new Gif("compiler-bot", "USERNAME", true, 1),
-        new Gif("book-dominos", "USERNAME", false, 3),
-        new Gif("ben-and-mike", "USERNAME", true, 1),
-        new Gif("android-explosion", "USERNAME", true, 1)
-);
+    public final static List<Gif> ALL_GIFS = Arrays.asList(
+            new Gif("infinite-andrew", "USERNAME", true, 1),
+            new Gif("cowboy-coder", "USERNAME", false, 2),
+            new Gif("compiler-bot", "USERNAME", true, 1),
+            new Gif("book-dominos", "USERNAME", false, 3),
+            new Gif("ben-and-mike", "USERNAME", true, 1),
+            new Gif("android-explosion", "USERNAME", true, 1)
+    );
 
-  public static List<Gif> getAllGifs(){
-      return ALL_GIFS;
-        }
-
-        public static List<Gif> getFavoriteGifs(){
-List<Gif> favoritesGifs = new ArrayList<>();
-for (Gif value : ALL_GIFS){
-    if(value.getFavorite() == true){
-        favoritesGifs.add(value);
+    public static List<Gif> getAllGifs() {
+        return ALL_GIFS;
     }
-}
-return favoritesGifs;
+
+    public static List<Gif> getFavoriteGifs() {
+        List<Gif> favoritesGifs = new ArrayList<>();
+        for (Gif value : ALL_GIFS) {
+            if (value.getFavorite() == true) {
+                favoritesGifs.add(value);
+            }
         }
+        return favoritesGifs;
+    }
+
+    public List<Gif> getGifssByCategoryId(int id) {
+        List<Gif> categoryGifs = new ArrayList<>();
+        for (Gif value:ALL_GIFS) {
+            if (value.getCategoryId() == id) {
+                categoryGifs.add(value);
+            }
+        }
+        return categoryGifs;
+    }
 }
